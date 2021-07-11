dd7b049d7ae8aa4768c3be09712481fab7601578
safi <safisafikuls@gmail.com>
2021-07-12 02:49:04 +0600

    updated my API KEY

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -10,7 +10,10 @@ export default function SearchMovies() {
     const searchMovies = async (e) => {
         e.preventDefault()
         
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`
+        // Not my API = 5dcf7f28a88be0edc01bbbde06f024ab
+        // My API = 794b4817c9c1bfeae86f3d13cc614f5b
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=794b4817c9c1bfeae86f3d13cc614f5b&language=en-US&query=${query}&page=1&include_adult=false`
 
         try {
             const res = await fetch(url)
